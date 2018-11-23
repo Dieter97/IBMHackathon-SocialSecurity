1bc7fede8a5cb1231bd1d99f45455f20ba141cba
Dieter Balemans <dieter.balemans@hotmail.com>
2018-11-23 19:50:05 +0100

    changes

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function loadPatientData() {
         success: function(patientDATA){
 
             if(debug){
-                patientDATA = "{\"id\":\"26\",\"patient_info\":{\"id\":\"26\",\"first_name\":\"Kirsten\",\"last_name\":\"Schuermans\",\"birthdate\":\"1988-08-25\",\"sex\":\"female\"},\"demand\":\"parking license\",\"pathologies\":[\"Ziekte van Alzheimer\"],\"keywords\":[{\"key\":\"heupprothese\",\"data_type\":\"text\",\"value\":\"2007: Totale heupprothese links\"},{\"key\":\"slechtziend\",\"data_type\":\"text\",\"value\":\"slechtziend\"},{\"key\":\"hypoxie\",\"data_type\":\"text\",\"value\":\"COPD GOLD III met emfyseem en bronchiëctasieën; reeds nachtelijke hypoxie in 2012 maar blijvende nicotine-abusus.\"},{\"key\":\"atelectase\",\"data_type\":\"text\",\"value\":\"Eind 2015 consultatie toegenomen hoesten en ook vermagering; CT thorax wat atelectase rechter MK.\"},{\"key\":\"exacerbatie\",\"data_type\":\"text\",\"value\":\"11-2016 COPD exacerbatie, sputumkweek: Moraxella en Haemofilus Influenzae\\nR/ Augmentin\"},{\"key\":\"pleuritis\",\"data_type\":\"text\",\"value\":\"Licht afgestompte longsinussen : sequelen pleuritis of lichtgradige hoeveelheid pleuravocht.\"}]}";
+                patientDATA = "{\"id\":\"26\",\"patient_info\":{\"id\":\"26\",\"first_name\":\"Kirsten\",\"last_name\":\"Schuermans\",\"birthdate\":\"1988-08-25\",\"sex\":\"female\"},\"demand\":\"parking license\",\"pathologies\":[\"Ziekte van Alzheimer\"],\"keywords\":[{\"key\":\"prothese\",\"data_type\":\"text\",\"value\":\"2007: Totale heupprothese links\"},{\"key\":\"slechtziend\",\"data_type\":\"text\",\"value\":\"zicht\"},{\"key\":\"hypoxie\",\"data_type\":\"text\",\"value\":\"COPD GOLD III met emfyseem en bronchiëctasieën; reeds nachtelijke hypoxie in 2012 maar blijvende nicotine-abusus.\"},{\"key\":\"atelectase\",\"data_type\":\"text\",\"value\":\"Eind 2015 consultatie toegenomen hoesten en ook vermagering; CT thorax wat atelectase rechter MK.\"},{\"key\":\"exacerbatie\",\"data_type\":\"text\",\"value\":\"11-2016 COPD exacerbatie, sputumkweek: Moraxella en Haemofilus Influenzae\\nR/ Augmentin\"},{\"key\":\"pleuritis\",\"data_type\":\"text\",\"value\":\"Licht afgestompte longsinussen : sequelen pleuritis of lichtgradige hoeveelheid pleuravocht.\"}]}";
             }
             patient = JSON.parse(patientDATA);
 
             //Set patient data on page
-            document.getElementById("patient-name").innerText = patient.patient_info.first_name + " " + patient.patient_info.first_name;
+            document.getElementById("patient-name").innerText = patient.patient_info.first_name + " " + patient.patient_info.last_name;
             document.getElementById("patient-birth").innerText = patient.patient_info.birthdate;
             document.getElementById("patient-gender").innerText = patient.patient_info.sex;
             document.getElementById("patient-profession").innerText = patient.patient_info.job;
@@ -39,7 +39,7 @@ function loadPatientData() {
             //Fill keyword list
             var html = "";
             patient.keywords.forEach(function(k) {
-                html += "<li class=\"list-group-item list-group-item-action\" onclick='loadKeyValue(k.key)'>"+k.key+"</li>";
+                html += "<li class='list-group-item list-group-item-action' onclick='loadKeyValue(\""+k.key+"\");'>"+k.key+"</li>";
             });
             document.getElementById("keyword-list").innerHTML = html;
 
@@ -52,5 +52,29 @@ function loadPatientData() {
 
 function loadKeyValue(key) {
     //TODO
+    console.log("test");
+    var text = "";
+    switch(key){
+        case "prothese":
+            text = "2007: Totale heupprothese links";
+            break;
+        case "slechtziend":
+            text = "slecht zien linker oog";
+            break;
+        case "hypoxie":
+            text = "Eind 2015 consultatie toegenomen hoesten en ook vermagering; CT thorax wat atelectase rechter MK.";
+            break;
+        case "atelectase":
+            text = "COPD GOLD III met emfyseem en bronchiëctasieën; reeds nachtelijke hypoxie in 2012 maar blijvende nicotine-abusus.";
+            break;
+        case "exacerbatie":
+            text = "11-2016 COPD exacerbatie, sputumkweek: Moraxella en Haemofilus Influenzae\nR/ Augmentin";
+            break;
+        case "pleuritis":
+            text = "Licht afgestompte longsinussen : sequelen pleuritis of lichtgradige hoeveelheid pleuravocht";
+            break;
+
+    }
+    document.getElementById("content").innerText = text;
 }
 
